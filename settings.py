2f71d72af17ca167f4a969ad81386923e0bdfa86
webQbe <webcubesolutionslk@gmail.com>
2024-12-29 16:14:25 +0530

    Created settings.py file

---
+++ settings.py
@@ -0,0 +1,6 @@
+STACKEXCHANGE_API_KEY = "YOUR_STACKEXCHANGE_API_KEY"
+SITE = "stackoverflow"
+RESULT_COUNT = 20  # Define result count per search
+
+''' Base URL for Stack Exchange API '''
+STACKEXCHANGE_SEARCH_URL = "https://api.stackexchange.com/2.3/search/advanced"
