add2b6c9027835cb5afbb2c0d6504543fbad749e
webQbe <webcubesolutionslk@gmail.com>
2025-10-10 15:00:23 +0530

    Updated settings.py to implement Google Search API

--- settings.py
+++ settings.py
@@ -1,6 +1,10 @@
-STACKEXCHANGE_API_KEY = "MY_STACKEXCHANGE_API_KEY"
-SITE = "stackoverflow"
-RESULT_COUNT = 20  # Define result count per search
+SEARCH_KEY = "MY_SEARCH_API_KEY"
+SEARCH_ID = "MY_SEARCH_ID"
+COUNTRY = "lk"
 
-''' Base URL for Stack Exchange API '''
-STACKEXCHANGE_SEARCH_URL = "https://api.stackexchange.com/2.3/search/advanced"
+''' Create URL to Call Get Search Results 
+    by Pass-in API Key, ID, Query, Result Page, & Country'''
+SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&gl=" + COUNTRY
+
+''' Define Result Count Per Search '''
+RESULT_COUNT = 20
